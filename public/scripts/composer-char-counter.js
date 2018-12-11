f104636eddd8bd223a7f17397e5ea8a4e4695460
Zixia Lu <zixialu@gmail.com>
2018-12-11 14:02:21 -0500

    Refactor element name for greater readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  // --- our code goes here ---
   $('.new-tweet textarea').keyup(function() {
     const remaining = 140 - $(this).val().length;
-    const remainingCounter = $(this).siblings('.counter')[0];
-    remainingCounter.innerText = remaining;
+    const characterCounter = $(this).siblings('.counter')[0];
+    characterCounter.innerText = remaining;
     if (remaining < 0) {
-      remainingCounter.classList.add('limit-exceeded');
+      characterCounter.classList.add('limit-exceeded');
     } else {
-      remainingCounter.classList.remove('limit-exceeded');
+      characterCounter.classList.remove('limit-exceeded');
     }
   });
 });
