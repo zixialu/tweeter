24f14bee1b0597886d113d2ba391e20d014e4200
Zixia Lu <zixialu@gmail.com>
2018-12-12 12:09:32 -0500

    Implement ajax requests to post and get tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,66 +29,42 @@ function createTweetElement(tweetData) {
   `;
 }
 
-/*
- * Loop through tweets, calling createTweetElement for each tweet and appends
- * it to the tweets container
- */
-function renderTweets(tweets) {
-  const $tweetsContainer = $('#tweets-container');
-  tweets.forEach(tweet => {
-    $tweetsContainer.append(createTweetElement(tweet));
-  });
-}
-
-// FIXME: Remove this later
-// Fake data taken from tweets.json
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: {
-        small: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        large: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png'
-      },
-      handle: '@SirIsaac'
-    },
-    content: {
-      text:
-        'If I have seen further it is by standing on the shoulders of giants'
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: {
-        small: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-        regular: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-        large: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png'
-      },
-      handle: '@rd'
-    },
-    content: {
-      text: 'Je pense , donc je suis'
-    },
-    created_at: 1461113959088
-  },
-  {
-    user: {
-      name: 'Johann von Goethe',
-      avatars: {
-        small: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
-        regular: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
-        large: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png'
-      },
-      handle: '@johann49'
-    },
-    content: {
-      text: 'Es ist nichts schrecklicher als eine tätige Unwissenheit.'
-    },
-    created_at: 1461113796368
+$(document).ready(function() {
+  /*
+   * Loop through tweets, calling createTweetElement for each tweet and appends
+   * it to the tweets container
+   */
+  function renderTweets(tweets) {
+    const $tweetsContainer = $('#tweets-container');
+    tweets.forEach(tweet => {
+      $tweetsContainer.append(createTweetElement(tweet));
+    });
   }
-];
 
-renderTweets(data);
+  // Post tweet to db
+  // TODO: Validation
+  const $form = $('#tweet-form');
+  $form.submit(event => {
+    event.preventDefault();
+    const serialData = $form.serialize();
+    console.log(serialData);
+
+    $.post('/tweets', serialData, () => {
+      console.log('Posted tweet');
+      loadTweets();
+    });
+  });
+
+  // Get tweets from db and pass them into renderTweets
+  /*
+   * TODO: Get only new tweets or else make renderTweets clear old tweets
+   * before rerendering
+   */
+  function loadTweets() {
+    $.get('/tweets', data => {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
+});
