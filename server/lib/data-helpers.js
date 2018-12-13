afd3be5ca3c05403910bba650307ed3e0236a6b7
Zixia Lu <zixialu@gmail.com>
2018-12-13 13:27:15 -0500

    Implement saveTweet with mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,35 +1,23 @@
 'use strict';
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require('./util/simulate-delay');
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-    // Saves a tweet to `db`
+    // Save a tweet to mongodb
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insertOne(newTweet);
+      callback(null, true);
     },
 
-    // Get all tweets in `db`, sorted by newest first
-    // getTweets: function(callback) {
-    //   simulateDelay(() => {
-    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-    //     callback(null, db.tweets.sort(sortNewestFirst));
-    //   });
-    // }
+    // Get a tweet from mongodb
     getTweets: function(callback) {
       db.collection('tweets')
         .find()
         .toArray((err, tweets) => {
-          console.log('getTweets' + tweets);
           if (err) {
             return callback(err);
           }
-          callback(null, tweets);
+          return callback(null, tweets);
         });
     }
   };
